4abf6da2b0a9e359cc934b2ec36022d4134d6fdc
Erik Huang <erikhuangdrums@gmail.com>
2021-01-03 20:42:43 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -132,7 +132,7 @@ def get_players(team)
   if team == nil 
     hash[:home][:players].concat(hash[:away][:players])
   end
-  hash[team][:players]
+  hash[team.to_s][:players]
 end
 
 def num_points_scored(name)
